60158e535b22fccf2cf07f2bff8ec976ba360183
Daniel <dbecker.fl@gmail.com>
2017-02-06 17:06:06 -0500

    Added Constants file

---
+++ HB141/Constants.swift
@@ -0,0 +1,24 @@
+//
+//  Constants.swift
+//  HB141
+//
+//  Created by Daniel Becker on 2/6/17.
+//  Copyright © 2017 International Human Trafficking Institute. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct ColorConstants {
+    //#9b59b6
+    let primaryColor = UIColor(red: 155, green: 89, blue: 182, alpha: 1.0)
+    
+    //#8e44ad
+    let secondaryColor = UIColor(red: 142, green: 68, blue: 173, alpha: 1.0)
+    
+    //#640e27
+    let highlightColor = UIColor(red: 100, green: 14, blue: 39, alpha: 1.0)
+    
+    //#282828
+    let textColor = UIColor(red: 40, green: 40, blue: 40, alpha: 1.0)
+}
