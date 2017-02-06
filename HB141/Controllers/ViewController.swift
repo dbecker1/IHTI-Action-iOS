93265e1b2f84d3d0d6a08ac6695dd7722e43bcdc
Daniel <dbecker.fl@gmail.com>
2017-02-05 19:59:32 -0500

    Initial Xcode Commit

---
+++ HB141/Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HB141
+//
+//  Created by Daniel Becker on 2/5/17.
+//  Copyright © 2017 International Human Trafficking Institute. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
