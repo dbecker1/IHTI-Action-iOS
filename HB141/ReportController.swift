3a924173c88e494d449fa8571bfccd060d6670bb
Tyler Abney <tabney00@gmail.com>
2017-02-28 14:43:45 -0500

    added submission form controller

---
+++ HB141/ReportController.swift
@@ -0,0 +1,35 @@
+//
+//  ReportController.swift
+//  HB141
+//
+//  Created by Tyler Abney on 2/23/17.
+//  Copyright © 2017 International Human Trafficking Institute. All rights reserved.
+//
+
+import UIKit
+
+class ReportController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
