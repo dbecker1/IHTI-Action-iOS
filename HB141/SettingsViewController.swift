368832dc7363be2b32d6ca08e3bc90faf667e3cb
Daniel <dbecker.fl@gmail.com>
2017-04-10 23:41:55 -0400

    Added menu support to settings page

--- HB141/SettingsViewController.swift
+++ HB141/SettingsViewController.swift
@@ -15,6 +15,17 @@ class SettingsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Settings";
+        
+        self.navigationController?.navigationBar.tintColor = UIColor.white;
+        navigationItem.leftBarButtonItem = BaseNavigationController.getMenuButton()
+        navigationItem.backBarButtonItem = BaseNavigationController.getBackButton()
+        
+        if self.revealViewController() != nil {
+            self.revealViewController().rearViewRevealWidth = 300
+            navigationItem.leftBarButtonItem?.target = self.revealViewController()
+            navigationItem.leftBarButtonItem?.action = #selector(SWRevealViewController.revealToggle(_:))
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
         // Do any additional setup after loading the view.
     }
 
